3888122d1ece064aec530bfff2b2b353ae144364
VolodymyrBovsunovskiy <102419980+VolodymyrBovsunovskiy@users.noreply.github.com>
2023-10-21 17:36:15 +0300

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import { Section } from "./Section/Section";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Statistics } from "./Statistics/Statistics";
+import { Notification } from "./Notification/Notification";
+
+export class App  extends Component{
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+
+  onLeaveFeedback = (option) => {
+this.setState((prevState) => {
+      return {
+        [option]: prevState[option] + 1,
+      }
+    }) 
+  }
+  onTotal = () =>  {
+      return( this.state.good + this.state.neutral  + this.state.bad)
+  }
+  positivePercentage = () => {
+    const total = this.onTotal();
+    const number = Math.round((this.state.good*100) / total);
+    return (
+   number
+    )
+  }
+
+  render() {
+    const {good,neutral, bad} = this.state;
+   
+    return (
+    <Section title={'Plese leave your feedback'}>
+        <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback} />
+        { this.onTotal() ?
+          <Statistics good={good} neutral={neutral} bad={bad} total={this.onTotal()} positivePercentage={this.positivePercentage()} />
+          : <Notification message={"There is no feedback"}/>}
+      </Section>
   );
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import styled from "styled-components";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode> 
 );
+export const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 20px;
+  max-width: 580px;
+
+ 
+  border-radius: 10px;
+`;
